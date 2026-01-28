c47655dd9eb0aa852d8d906ebd1ba920a6bf95c3
zyanv <azammuddinazrul18@gmail.com>
2026-01-29 00:33:44 +0800

    Add files via upload

---
+++ .github/workflows/main.py
@@ -0,0 +1,29 @@
+# Welcome to Python Code Pad
+# Code is automatically saved on edit
+# Use it to run Python 3 code
+
+# Print to the console
+print('Python is awesome')
+
+# Write functions
+def greeting():
+    return 'Hello'
+
+# Call functions
+print(greeting())
+
+# Import from Standard Library
+import datetime
+now = datetime.datetime.now()
+print(now)
+
+# Create and work with classes
+class Greeting:
+    def greet(self, name):
+        return 'Hello ' + name
+
+print(Greeting().greet('Developer'))
+
+# Have fun coding in Python
+
+i havee
